360d9fe5663626a2913ddaae4683dd7d3ae4f72f
Haitham-IBM-UOP <haithamyousdif@gmail.com>
2026-08-12 23:51:27 +0300

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,1 +1,23 @@
+import json
+import requests
 
+
+def emotion_detector(text_to_analyze):
+  url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+  headers = {"grpc-metadata-mm-model-id": "emotion_single_bert-3.8-base"}
+  myobj = {"raw_document": {"text": text_to_analyze}}
+
+  response = requests.post(url, json=myobj, headers=headers)
+  formatted_response = json.loads(response.text)
+
+  emotions = formatted_response["emotionPredictions"][0]["emotion"]
+  dominant_emotion = max(emotions, key=emotions.get)
+
+  return {
+      "anger": emotions["anger"],
+      "disgust": emotions["disgust"],
+      "fear": emotions["fear"],
+      "joy": emotions["joy"],
+      "sadness": emotions["sadness"],
+      "dominant_emotion": dominant_emotion,
+  }
